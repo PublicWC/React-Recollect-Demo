556ac4d4d7500661a5daa1cc8d613731afcc39fc
Chris Wong <wc38.wong.public@gmail.com>
2019-01-22 10:30:29 +0800

    Updates

---
+++ precache-manifest.e341d2f23b6314ce59a65d9ec94a02af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1cb4af1640fc32d3289",
+    "url": "//React-Recollect-Demo/static/css/main.527b69b8.chunk.css"
+  },
+  {
+    "revision": "c1cb4af1640fc32d3289",
+    "url": "//React-Recollect-Demo/static/js/main.c1cb4af1.chunk.js"
+  },
+  {
+    "revision": "082eaef2d69d7da97509",
+    "url": "//React-Recollect-Demo/static/js/1.082eaef2.chunk.js"
+  },
+  {
+    "revision": "446e7e3565bce921b567",
+    "url": "//React-Recollect-Demo/static/js/runtime~main.446e7e35.js"
+  },
+  {
+    "revision": "45850f8b224c6a6be590cf86b5a6ad24",
+    "url": "//React-Recollect-Demo/index.html"
+  }
+];
