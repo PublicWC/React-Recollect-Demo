092267a06d9415b148a3b93f6a3fa1ac85021388
Chris Wong <wc38.wong.public@gmail.com>
2019-01-22 10:31:51 +0800

    Updates

---
+++ precache-manifest.83889afbb8ae45e3126292a744f8aa00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1cb4af1640fc32d3289",
+    "url": "/React-Recollect-Demo/static/css/main.527b69b8.chunk.css"
+  },
+  {
+    "revision": "c1cb4af1640fc32d3289",
+    "url": "/React-Recollect-Demo/static/js/main.c1cb4af1.chunk.js"
+  },
+  {
+    "revision": "082eaef2d69d7da97509",
+    "url": "/React-Recollect-Demo/static/js/1.082eaef2.chunk.js"
+  },
+  {
+    "revision": "b7d22ee7c7fab631efe3",
+    "url": "/React-Recollect-Demo/static/js/runtime~main.b7d22ee7.js"
+  },
+  {
+    "revision": "ab8da13bf0f1015fabd91c048f75c22f",
+    "url": "/React-Recollect-Demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "//React-Recollect-Demo/precache-manifest.e341d2f23b6314ce59a65d9ec94a02af.js"
+  "/React-Recollect-Demo/precache-manifest.83889afbb8ae45e3126292a744f8aa00.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("//React-Recollect-Demo/index.html", {
+workbox.routing.registerNavigationRoute("/React-Recollect-Demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
